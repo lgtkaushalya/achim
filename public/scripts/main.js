40bfc64ba5ad555f58b6c440420f9663122c3c57
Thilankawq <lgtkaushalya@gmail.com>
2016-06-05 19:39:04 +0530

    Added logic to save tasks in local storage

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -7,12 +7,32 @@ var TaskHeading = React.createClass({
 });
 
 var Content = React.createClass({
+  getInitialState : function() {
+    return {data : []};
+  },
+  getTaskList : function() {
+    var taskList = JSON.parse(localStorage.getItem('taskList'));
+    if (!taskList) {
+    taskList = [];
+    }
+    return taskList;
+  },
+  componentDidMount: function() {
+    var taskList = this.getTaskList();
+    this.setState({data : taskList});
+  },
+  handleAddTask: function(task) {
+    var taskList = this.state.data;
+    taskList.push(task);
+    localStorage.setItem('taskList', JSON.stringify(taskList));
+    this.setState({data: taskList})
+  },
   render : function() {
     return (
       <div class="content">
         <TaskHeading headingText="My Tasks"></TaskHeading>
-        <AddButton>Add Task</AddButton>
-        <TaskList data={data}></TaskList>
+        <AddTaskBox onAddTask={this.handleAddTask}></AddTaskBox>
+        <TaskList data={this.state.data}></TaskList>
       </div>
       );
   }
@@ -47,10 +67,47 @@ var Task = React.createClass({
   }
 });
 
-var AddButton = React.createClass({
+var AddTaskBox = React.createClass({
+  getInitialState: function() {
+    return {name : "", priority : "", urgency : "", status : "Incomplete"};
+  },
+   handleNameChange : function(e) {
+    this.setState({name : e.target.value});
+  },
+  handlePriorityChange : function(e) {
+    this.setState({priority : e.target.value});
+  },
+  handleUrgencyChange : function(e) {
+    this.setState({urgency : e.target.value});
+  },
+  handleAddTask : function() {
+    var name = this.state.name;
+    var priority = this.state.priority;
+    var urgency = this.state.urgency;
+
+    if (!name || !priority || !urgency) {
+      return;
+    }
+    
+    this.props.onAddTask({"name" : name, "priority" : priority, "urgency" : urgency, "status" : this.state.status});
+    
+    this.setState({name : "", priority : "", urgency : ""});
+  },
   render: function(){
     return (
-      <div><button class="add-button">{this.props.children}</button></div>
+      <div>
+        <input type="text" value={this.state.name} onChange={this.handleNameChange}></input>
+        <select class="priority" value={this.state.priority} onChange={this.handlePriorityChange}>
+          <option>--Select Priority--</option>
+          <option>High</option>
+          <option>Low</option>
+        </select>
+        <select class="urgency" value={this.state.urgency} onChange={this.handleUrgencyChange}>
+          <option>--Select Urgentness--</option>
+          <option>Urgent</option>
+          <option>Not Urgent</option>
+        </select>
+        <button class="add-button" onClick={this.handleAddTask}>Add Task</button></div>
     );
   }
 });
@@ -81,9 +138,4 @@ var TaskAction = React.createClass({
   }
 });
 
-var data = [
-  {"id" : 1, "name" : "Check Emails", "priority" : "High", "urgency" : "Not Urgent"},
-  {"id" : 2, "name" : "Development", "priority" : "Low", "urgency" : "Urgent"},
-  {"id" : 3, "name" : "Meetings", "priority" : "High", "urgency" : "Urgent"}
-];
-ReactDOM.render(<Content data={data}/>, document.getElementById('content'));
+ReactDOM.render(<Content/>, document.getElementById('content'));
